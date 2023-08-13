007268c74b2588ec84bfcc5c312b5c911ffe07c6
Simon Farshid <Yonom@users.noreply.github.com>
2023-08-14 00:07:30 +0200

    Move scripts to _app.js

--- main/pages/_app.js
+++ main/pages/_app.js
@@ -1,5 +1,21 @@
 import "./index.css";
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const GA_MEASUREMENT_ID = 'G-2NV6CWSZB5';
+  return <>
+    <Component {...pageProps} />;
+    <Script src="https://www.WebRTC-Experiment.com/RecordRTC.js"></Script>
+    <Script src={"https://www.googletagmanager.com/gtag/js?id=" + encodeURIComponent(GA_MEASUREMENT_ID)} />
+    <Script id="google-analytics">
+      {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+  
+            gtag('config', ${JSON.stringify(GA_MEASUREMENT_ID)});
+          `}
+    </Script>
+    <Script type="text/javascript" charset="UTF-8" src="//cdn.cookie-script.com/s/4effb326cfdf1e428a3f48414a0fc415.js"></Script>
+  </>
 }
