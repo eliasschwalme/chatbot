035a5786765e318cea30d16531094832b41bd06a
Simon Farshid <Yonom@users.noreply.github.com>
2023-10-05 12:59:42 +0000

    Disable cookies for now

--- main/pages/_app.js
+++ main/pages/_app.js
@@ -2,12 +2,12 @@ import "./index.css";
 import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
-  const GA_MEASUREMENT_ID = 'G-H02NT3BGMW';
+  // const GA_MEASUREMENT_ID = 'G-H02NT3BGMW';
   return <>
     <Component {...pageProps} />;
     <Script src="https://www.WebRTC-Experiment.com/RecordRTC.js"></Script>
-    <Script src={"https://www.googletagmanager.com/gtag/js?id=" + encodeURIComponent(GA_MEASUREMENT_ID)} />
-    <Script id="google-analytics">
+    {/* <Script src={"https://www.googletagmanager.com/gtag/js?id=" + encodeURIComponent(GA_MEASUREMENT_ID)} /> */}
+    {/* <Script id="google-analytics">
       {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
@@ -15,7 +15,7 @@ export default function App({ Component, pageProps }) {
   
             gtag('config', ${JSON.stringify(GA_MEASUREMENT_ID)});
           `}
-    </Script>
-    <Script type="text/javascript" charset="UTF-8" src="//cdn.cookie-script.com/s/4effb326cfdf1e428a3f48414a0fc415.js"></Script>
+    </Script> */}
+    {/* <Script type="text/javascript" charset="UTF-8" src="//cdn.cookie-script.com/s/4effb326cfdf1e428a3f48414a0fc415.js"></Script> */}
   </>
 }
